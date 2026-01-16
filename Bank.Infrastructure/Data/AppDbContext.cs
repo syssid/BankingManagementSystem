9ec177a69ec67b976f53151979eb1d7c7e9cc50c
Sidhartha Behera <sidhartha.behera@r-pac.com>
2026-01-16 22:26:31 +0530

    added-login-functionality

---
+++ Bank.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Bank.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Infrastructure.Data
+{
+    internal class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<ApplicationUser> Users { get; set; }
+    }
+}
