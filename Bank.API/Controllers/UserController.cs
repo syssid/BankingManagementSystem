9d18b49b17abb5d224b0e9c1bbdd1d4142563c1f
Sidhartha Behera <sidhartha.behera@r-pac.com>
2026-01-16 23:33:04 +0530

    added-user-controller

---
+++ Bank.API/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using Bank.Application.Contracts;
+using Bank.Application.DTO.Request;
+using Bank.Application.DTO.Response;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bank.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUser user;
+
+        public UserController(IUser user)
+        {
+            this.user = user;
+        }
+        [HttpPost("login")]
+        public async Task<ActionResult<LoginResponse>> LoginUser(LoginDTO loginDTO)
+        {
+            var result = await user.LoginUserAsync(loginDTO);
+
+            return Ok(result);
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<RegistrationResponse>> RegisterUser(RegisterUserDTO registerUserDTO)
+        {
+            var result = await user.RegisterUserAsync(registerUserDTO);
+
+            return Ok(result);
+        }
+    }
+}
