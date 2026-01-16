659b9721361ee58b294f3571b3f6d5af31887114
Sidhartha Behera <sidhartha.behera@r-pac.com>
2026-01-16 22:49:16 +0530

    added-connection-string-to-user-secrets

--- Bank.API/Program.cs
+++ Bank.API/Program.cs
@@ -1,6 +1,10 @@
+using Bank.Infrastructure.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.InfrastructureServices(builder.Configuration);
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
