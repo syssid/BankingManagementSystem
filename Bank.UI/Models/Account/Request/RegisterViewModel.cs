309b4dbf7bfed31c60533c08dddb5d0f83ab7625
Sidhartha Behera <sidhartha.behera@r-pac.com>
2026-01-17 20:38:14 +0530

    login-registration-ui-api-done

---
+++ Bank.UI/Models/Account/Request/RegisterViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bank.UI.Models.Account.Request
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public required string FullName { get; set; }
+
+        [Required, EmailAddress]
+        public required string Email { get; set; }
+
+        [Required, MinLength(6)]
+        public required string Password { get; set; }
+
+        [Compare("Password")]
+        public required string ConfirmPassword { get; set; }
+    }
+}
