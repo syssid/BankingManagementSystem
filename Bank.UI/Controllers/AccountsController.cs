86ddf4e1c4ea084629f35ae2ea306335d97558ed
Sidhartha Behera <sidhartha.behera@r-pac.com>
2026-01-20 22:26:19 +0530

    aaded-controller-snippets

---
+++ Bank.UI/Controllers/AccountsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bank.UI.Controllers
+{
+    public class AccountsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
